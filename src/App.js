2af1e4b080bed0e87abb661e4f39f9d222060e52
Nuno Vieira <nuno.vieira@itcenter.com.pt>
2019-02-26 12:18:38 +0000

    What is supposed to be buildQueryFactory?
    Unhandled Promise Rejection: TypeError: buildQueryFactory is not a function. (In 'buildQueryFactory(introspectionResults, otherOptions)', 'buildQueryFactory' is undefined)

--- src/App.js
+++ src/App.js
@@ -1,20 +1,62 @@
-import React from 'react';
+import React, { Component } from 'react';
+import buildGraphQLProvider from 'ra-data-graphql';
 import { Admin, Resource } from 'react-admin';
 import { PostList, PostEdit, PostCreate } from './posts';
 import { UserList } from './users';
-import jsonServerProvider from 'ra-data-json-server';
+// import jsonServerProvider from 'ra-data-json-server';
 import PostIcon from '@material-ui/icons/Book';
 import UserIcon from '@material-ui/icons/Group';
 import Dashboard from './Dashboard';
 import authProvider from './authProvider';
 // import dataProvider from './dataProvider';
 
-const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
-const App = () => (
-  <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
-      <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
-      <Resource name="users" list={UserList} icon={UserIcon} />
-  </Admin>
-);
+// const dataProvider = jsonServerProvider('http://jsonplaceholder.typicode.com');
+// const App = () => (
+//   <Admin dashboard={Dashboard} authProvider={authProvider} dataProvider={dataProvider}>
+//       <Resource name="posts" list={PostList} edit={PostEdit} create={PostCreate} icon={PostIcon} />
+//       <Resource name="users" list={UserList} icon={UserIcon} />
+//   </Admin>
+// );
 
+class App extends Component {
+  constructor() {
+    super();
+    this.state = { dataProvider: null };
+  }
+  componentDidMount() {
+    const options = {
+      clientOptions: {
+        uri: 'http://localhost:4002'
+      }
+    };
+    buildGraphQLProvider(options).then(dataProvider =>
+      this.setState({ dataProvider })
+    );
+  }
+
+  render() {
+    const { dataProvider } = this.state;
+
+    if (!dataProvider) {
+      return <div>Loading</div>;
+    }
+
+    return (
+      <Admin
+        dashboard={Dashboard}
+        authProvider={authProvider}
+        dataProvider={dataProvider}
+      >
+        <Resource
+          name="posts"
+          list={PostList}
+          edit={PostEdit}
+          create={PostCreate}
+          icon={PostIcon}
+        />
+        <Resource name="users" list={UserList} icon={UserIcon} />
+      </Admin>
+    );
+  }
+}
 export default App;
